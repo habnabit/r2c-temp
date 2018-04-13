a2ee514ddd8736844ee539ed568108dd7593d31b
Derrick Liu <dliu@lediur.com>
2018-04-13 13:08:30 -0700

    HelloScreen and its ilk

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,77 @@
+import { Classes, Button } from '@blueprintjs/core';
 import * as React from 'react';
 import './App.css';
+import 'normalize.css/normalize.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
-import logo from './logo.svg';
+interface AppState {
+  isAuthenticated: boolean;
+  user?: string;
+  docId?: string;
+}
 
-class App extends React.Component {
-  public render() {
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false,
+    };
+  }
+
+  componentDidMount() {}
+
+  submitNewName = (newName: string) => {};
+
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {!this.state.isAuthenticated && (
+          <HelloScreen submitNewName={this.submitNewName} />
+        )}
+      </div>
+    );
+  }
+}
+
+interface HelloProps {
+  submitNewName: (newName: string) => void;
+}
+
+interface HelloState {
+  name: string;
+}
+
+class HelloScreen extends React.Component<HelloProps, HelloState> {
+  constructor(props: HelloProps) {
+    super(props);
+    this.state = {
+      name: '',
+    };
+  }
+
+  private onSubmitName() {
+    this.props.submitNewName(this.state.name);
+  }
+
+  private onChangeName(e: React.FormEvent<HTMLInputElement>) {
+    this.setState({ name: e.currentTarget.value });
+  }
+
+  public render() {
+    return (
+      <div className="screen hello-screen">
+        <h1>Hi!</h1>
+        <input
+          className={Classes.INPUT}
+          type="text"
+          placeholder="Enter your name"
+          dir="auto"
+          value={this.state.name}
+          onChange={this.onChangeName}
+        />
+        <Button onClick={this.onSubmitName}>Sign in</Button>
       </div>
     );
   }
