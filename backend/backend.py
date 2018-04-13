b620eb0ae6bd7abd34007f0f0d13f9f019f99288
r2c <r2c@r2cs-MacBook-Pro.local>
2018-04-13 13:30:37 -0700

    Add more endpoints.
    Also adjust to camelCase methods.

--- backend/backend.py
+++ backend/backend.py
@@ -2,8 +2,10 @@ from zope.interface import Interface, Attribute, implementer
 from twisted.python.components import registerAdapter
 from twisted.web.server import Session
 import attr
+import base64
 import json
 import klein
+import os
 
 
 class IUser(Interface):
@@ -17,11 +19,26 @@ class User(object):
     name = attr.ib(default=None)
 
     @classmethod
-    def from_session(cls, session):
+    def fromSession(cls, session):
         return cls()
 
 
-registerAdapter(User.from_session, Session, IUser)
+registerAdapter(User.fromSession, Session, IUser)
+
+
+@attr.s()
+class Document(object):
+    id = attr.ib()
+    content = attr.ib()
+    users = attr.ib(default=attr.Factory(set))
+    owner = attr.ib(default=None)
+
+    def asJSON(self, viewer):
+        return {
+            'content': self.content,
+            'otherUsers': list(self.users - {viewer}),
+            'youAreOwner': self.owner == viewer,
+        }
 
 
 def asJSON(request, obj):
@@ -39,7 +56,7 @@ class DocStore(object):
 
     app = klein.Klein()
 
-    @app.route('/api/doc')
+    @app.route('/api/doc', methods=['GET'])
     def api_docs(self, request):
         return asJSON(request, self.documents.keys())
 
@@ -49,15 +66,36 @@ class DocStore(object):
 
     @app.route('/api/whoami', methods=['PUT'])
     def api_whoami_set(self, request):
-        content = json.load(request.content)
-        if content['user'] in self.users:
+        body = json.load(request.content)
+        if body['user'] in self.users:
             request.setResponseCode(409)
             return asJSON(request, {})
 
         user = self.whoami(request)
-        user.name = content['user']
+        user.name = body['user']
         self.users[user.name] = user
         return asJSON(request, {'user': user.name})
+
+    @app.route('/api/doc', methods=['POST'])
+    def api_doc_create(self, request):
+        body = json.load(request.content)
+        doc = Document(content=body['content'], 
+                       id=base64.b64encode(os.urandom(9)))
+        self.documents[doc.id] = doc
+        return asJSON(request, {'created': True, 'id': doc.id})
+
+    @app.route('/api/doc/<docId>')
+    def api_docs(self, request, docId):
+        doc = self.documents.get(docId)
+        if doc is None:
+            request.setResponseCode(404)
+            return asJSON(request, {})
+        else:
+            user = self.whoami(request)
+            if doc.owner is None:
+                doc.owner = user
+            doc.users.add(user)
+            return asJSON(request, doc.asJSON(user))
 
 
 DocStore().app.run('localhost', 7070)
